81fdf56d98f3db247e7bd7654d31cba1f4ae10a9
Nick <Nickjv2000@hotmail.nl>
2023-02-01 13:00:34 +0100

    base files
    base

---
+++ MPA/resources/views/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MPA</title>
+
+        <h1><center>Multi Page Appli - Home</center></h1>
+
+        
+    </body>
+</html>
---
+++ MPA/resources/views/welcome.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <h1><center>Multi Page Appli</center></h1>
+
+        <center><a href="home.blade.php">Home</a></center>
+    </body>
+</html>
