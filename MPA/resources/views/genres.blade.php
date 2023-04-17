154489b8cca272869c24789c85de33ada93d8dab
Nick <nickjv2000@hotmail.nl>
2023-04-17 13:25:03 +0200

    Fix + add on songs (numeric)
    The code is working now and added a numeric for the songs since ill be adding multiple

--- MPA/resources/views/genres.blade.php
+++ MPA/resources/views/genres.blade.php
@@ -8,17 +8,17 @@ $id = request('id');
 
     @foreach($genreAll as $genres) 
         <h2>
-            <!-- <center><a href="/genres/{{$genres['id']}}">{{$genres['title']}}</a></center> -->
+            <center><a href="/genres/{{$genres['id']}}">{{$genres['title']}}</a></center>
         </h2>
     @endforeach
     @else()
- <!--        <h1>
+       <h1>
             <center><a href="/genres">Home</a></center>
         </h1>
         <h2>
             <center>{{$genre['title']}}</center>
         </h2>
         <h3>
-            <center>{{$genre['songs']}}</center>
-        </h3> -->
+            <center>{{$genre['id']}}. {{$genre['songs']}}</center>
+        </h3> 
 @endif
